9558f9e13b687b00fdf36b2dbaf77695eec201dd
aswinzz <iit2016106@iiita.ac.in>
2019-05-24 17:22:05 +0530

    add readme

--- extension/public/api.js
+++ extension/public/api.js
@@ -1,5 +1,5 @@
 $(function(){
-var url= 'http://localhost:8000/api';
+var url= 'https://rancode.herokuapp.com/api';
   requestJSON(url, function(response) {
     console.log(response);
     var output = '<div class="col-md-10 col-md-offset-1"><div class="home-content"><h1 class="white-text"><a target="_blank" style="color:white" href="'+response.data.url+'">'+response.data.name+'</a></h1><button class="main-btn">Type - '+response.data.type+'</button><a target="_blank" href="'+response.data.submit_url+'"><button class="main-btn">Code - '+response.data.code+'</button></a><button class="main-btn">Submissions - '+response.data.submissions+'</button><a target="_blank" href="'+response.data.status+'"><button class="main-btn">Accuracy - '+response.data.accuracy+'</button></a></div></div>' 
---
+++ scraper/README.md
@@ -0,0 +1,6 @@
+# RANCODE
+<div class="logos"><img src="./extension/public/img/mylogo.png" width="220px" align="right"></div>
+A new tab extension that suggests random coding problem for you to solve. Get motivated and keep solving!
+
+## Screenshot
+<div class="logos"><img src="./extension/public/img/ss.png"></div>
